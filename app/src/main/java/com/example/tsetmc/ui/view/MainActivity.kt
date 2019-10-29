45a8394b6ff51f58fb85538859c987330966e31a
Iman Ahrari <imi2000.ia@gmail.com>
2019-10-29 18:52:28 +0330

    Add JalaliCalendar to project

--- app/src/main/java/com/example/tsetmc/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/tsetmc/ui/view/MainActivity.kt
@@ -3,50 +3,15 @@ package com.example.tsetmc.ui.view
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
+import android.util.Log
 import com.example.tsetmc.R
-import com.example.tsetmc.service.model.Market
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
+import com.example.tsetmc.service.utils.JalaliCalendar
 
 @SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
 
-//    private val job = Job()
-//    private val scope = CoroutineScope(Dispatchers.Main + job)
-//    private val list: MutableList<Market> = ArrayList()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        scope.launch {
-//            //list.addAll(retrieveDataList()).apply { sortList() }
-//            data_list_text.text = list.toString()
-//            to_edit_text.setOnKeyListener { _, keyCode, event ->
-//                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-//                    val from = from_edit_text.text.toString().toFloat()
-//                    val to = to_edit_text.text.toString().toFloat()
-//                    val filteredList = filterList(from, to)
-//
-//                    data_list_text.text = filteredList.toString()
-//                    true
-//                } else false
-//            }
-//        }
     }
-
-
-
-    private fun sortList(){
-        val list = arrayListOf<Market>()
-        list.sortWith(Comparator { o1, o2 ->
-            ((o1.lastTransactionPercent!! - o2.lastTransactionPercent!!) * 1000.0).toInt()
-        })
-    }
-
-//    private fun filterList(from: Float, to: Float): List<Market>{
-//        return list.filter {
-//            from <= it.lastTransactionPercent!! && to >= it.lastTransactionPercent
-//        }
-//    }
 }
--- app/src/test/java/com/example/tsetmc/ExampleUnitTest.kt
+++ app/src/test/java/com/example/tsetmc/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.tsetmc
 
+import com.example.tsetmc.service.utils.JalaliCalendar
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +15,19 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val calendar = JalaliCalendar()
+
+        val year = calendar.get(JalaliCalendar.YEAR)
+        val month = calendar.get(JalaliCalendar.MONTH)
+        val day = calendar.get(JalaliCalendar.DAY_OF_MONTH)
+
+        val convert = JalaliCalendar()
+        convert.time = calendar.time
+        val hour = convert.get(JalaliCalendar.YEAR)
+        val minute = convert.get(JalaliCalendar.MONTH)
+        val second = convert.get(JalaliCalendar.DAY_OF_MONTH)
+        val date = "$year/${month}/$day"
+
+        assertEquals(Date().time, JalaliCalendar().time.time)
     }
 }
